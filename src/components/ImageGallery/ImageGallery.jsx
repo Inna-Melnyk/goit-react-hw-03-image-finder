a4dacd17dae750ff014d9c377f7847b8f0d16fb6
Inna-Melnyk <inamelnyk@ukr.net>
2023-06-20 00:12:34 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -8,117 +8,61 @@ import { Container, Text } from './ImageGallery.styled';
 
 export class ImageGallery extends Component {
   state = {
-    searchName: '',
-    images: null,
+    images: [],
     page: 1,
     loading: false,
-    loadbutton: false,
+    hasMoreImages: false,
   };
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps) {
     if (prevProps.searchImage !== this.props.searchImage) {
-      this.setState({
-        loading: true,
-        page: 1,
-        images: null,
-        loadbutton: false,
-      });
-
-      fetchGalleryPictures({ query: this.props.searchImage })
-        .then(data => {
-          if (data.hits.length === 0 || data === undefined) {
-            toast.error(
-              'Sorry, there are no images matching your search query. Please try again.'
-            );
-            this.setState({
-              loading: false,
-              loadbutton: false,
-            });
-            return;
-          }
-          if (data.hits.length < 12) {
-            return this.setState({
-              loadbutton: false,
-              loading: false,
-            });
-          }
-          this.setState({
-            searchName: this.props.searchImage,
-            images: data.hits,
-            loading: false,
-            loadbutton: true,
-          });
-        })
-        .catch(message => {
-          toast.error(`Sorry, there is no more picture`);
-          this.setState({
-            loadbutton: false,
-            loading: false,
-          });
-        });
-    }
-
-    if (prevState.page !== this.state.page) {
-      fetchGalleryPictures({
-        query: this.state.searchName,
-        pageNumber: this.state.page,
-      })
-        .then(data => {
-          if (data.hits.length < 12) {
-            toast.warn(`That's the lest batch of pictures`);
-            return this.setState(prevState => {
-              return {
-                images: [...prevState.images, ...data.hits],
-                loading: false,
-                loadbutton: false,
-              };
-            });
-          }
-
-          this.setState(prevState => {
-            return {
-              images: [...prevState?.images, ...data.hits],
-              loading: false,
-              loadbutton: true,
-            };
-          });
-        })
-        .catch(() => {
-          toast.error(`Sorry, there is no more picture`);
-          this.setState({
-            loadbutton: false,
-            loading: false,
-          });
-        });
+      this.setState({ images: [], page: 1 }, this.fetchImages);
     }
   }
 
-  handleLoadMoreImages = () => {
-    this.setState(prevState => {
-      return {
-        loading: true,
-        page: prevState.page + 1,
-        loadbutton: false,
-      };
-    });
+  fetchImages = () => {
+    const { searchImage } = this.props;
+    const { page } = this.state;
+
+    this.setState({ loading: true, hasMoreImages: false });
+
+    fetchGalleryPictures({ query: searchImage, pageNumber: page })
+      .then(data => {
+        if (!data.hits.length) {
+          return toast.error(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        }
+
+        if (data.hits.length < 12) {
+          toast.warn(`That's the lest batch of pictures`);
+        }
+
+        this.setState(prevState => ({
+          images: [...prevState.images, ...data.hits],
+          page: prevState.page + 1,
+          hasMoreImages: data.hits.length >= 12,
+        }));
+      })
+      .catch(message => {
+        toast.error(`Sorry, there is no more picture`);
+      })
+      .finally(() => this.setState({ loading: false }));
   };
 
-    render() {
-        const { images, loadbutton, loading } = this.state;
-        const { searchImage } = this.props;
+  handleLoadMoreImages = () => {
+    this.fetchImages();
+  };
+
+  render() {
+    const { images, hasMoreImages, loading } = this.state;
+    const { searchImage } = this.props;
     return (
       <Container>
         {!images && <Text>Enter search name</Text>}
 
-        {images && (
-          <ImageGalleryList
-            items={images}
-            name={searchImage}
-          />
-        )}
-        {loadbutton && (
-          <Button onLoadMore={this.handleLoadMoreImages} />
-        )}
+        {images && <ImageGalleryList items={images} name={searchImage} />}
+        {hasMoreImages && <Button onLoadMore={this.handleLoadMoreImages} />}
         {loading && <Loader />}
       </Container>
     );
